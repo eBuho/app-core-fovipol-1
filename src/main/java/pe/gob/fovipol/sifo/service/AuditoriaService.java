ad22ff494894ce2915cf22c7cc090f7ad8f2ce89
luisbellido <luis.bellido2010@gmail.com>
2014-11-04 16:01:27 -0500

    Deleted Files

---
+++ src/main/java/pe/gob/fovipol/sifo/service/AuditoriaService.java
@@ -0,0 +1,8 @@
+package pe.gob.fovipol.sifo.service;
+
+public interface AuditoriaService {
+    // Obtiene el IP del equipo cliente
+    public String obtenerIP();
+    // Obtiene el Nombre del equipo cliente
+    public String obtenerEquipo();
+}
